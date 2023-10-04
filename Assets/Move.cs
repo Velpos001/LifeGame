c4ba2ac2d2fee79dd9695638dd53125ee3eebda0
RA <draco.ryu.vantan@gmail.com>
2023-10-04 18:16:46 +0900

    week2

---
+++ Assets/Move.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] Rigidbody _rigidBody;
+    bool isStop = false;
+    [SerializeField] int _Speed;
+
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        
+        
+        
+        //TODO
+        //ˆÚ“®‚¹‚æ
+        //https://candle-stoplight-544.notion.site/4e021f226d584730b715626436ccc330
+
+    }
+}
